4a279a14aceae9a73eabedf106ee1192c0cac8b3
tom1299 <tom1299@jeeatwork.com>
2023-10-04 11:59:48 +0200

    Fix typo in result

--- code-generation-from-bdd/steps/steps.py
+++ code-generation-from-bdd/steps/steps.py
@@ -1,6 +1,3 @@
-# Prompt: Implement the steps for this specification using the file calculator.py
-import re
-
 from behave import given, when, then
 from calculator import Calculator
 
@@ -8,10 +5,10 @@ from calculator import Calculator
 def step_impl(context):
   context.calculator = Calculator()
 
-@when(u'I call the method calculate with the parameters {number_a:d}, {number_b:d} and the {sign}')
+@when('I call the method calculate with the paramenters {number_a:d}, {number_b:d} and the {sign}')
 def step_impl(context, number_a, number_b, sign):
   context.result = context.calculator.calculate(number_a, number_b, sign)
 
-@then(u'Then the method should return {result:d}')
+@then('the method should return {result:d}')
 def step_impl(context, result):
   assert context.result == result
